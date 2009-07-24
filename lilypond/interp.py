9a41a2a0be404a202d9041ebabfce785e5002ba2
James Tauber <jtauber@jtauber.com>
2009-07-24 03:19:53 -0400

    implementation of octaves passes second test

--- lilypond/interp.py
+++ lilypond/interp.py
@@ -1,3 +1,5 @@
+import re
+
 MIDI_NOTE_VALUES = {
     "c": 0,
     "d": 2,
@@ -14,12 +16,26 @@ def tokenize(s):
 
 def parse(s):
     duration = 16
-    octave = 4
+    curr_octave = 4
     offset = 0
     
     for token in tokenize(s):
-        if token in "abcdefg":
-            yield (offset, MIDI_NOTE_VALUES[token] + (12 * octave), duration)
+        m = re.match("(?P<note>[abcdefg])(?P<octave>'+|,+)?", token)
+        if m:
+            note = m.groupdict()["note"]
+            octave_marker = m.groupdict()["octave"]
+            if octave_marker == "'":
+                octave = curr_octave + 1
+            elif octave_marker == "''":
+                octave = curr_octave + 2
+            elif octave_marker == ",":
+                octave = curr_octave - 1
+            elif octave_marker == ",,":
+                octave = curr_octave - 2
+            else:
+                octave = curr_octave
+            note_value = MIDI_NOTE_VALUES[note] + (12 * octave)
+            yield (offset, note_value, duration)
             offset += duration
         else:
             raise Exception("unsupported token %s" % token)
