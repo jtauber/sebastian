690bdc2cbb5aed0afcb466d771c9eae98dd8d603
Nicholas Tollervey <ntoll@ntoll.org>
2009-08-08 18:37:55 +0800

    From yesterday evening... added handling of sharps and flats. Also had to correct minor mistakes in tests and attempted to simplify how the regex appear in the code (for ease of use)
    Signed-off-by: James Tauber <jtauber@jtauber.com>

--- lilypond/interp.py
+++ lilypond/interp.py
@@ -10,6 +10,14 @@ MIDI_NOTE_VALUES = {
     "b": 11,
 }
 
+# Cutting up the regex into manageable components - helps with testing
+RE_NOTE = "(?P<note>[abcdefg])"
+RE_8VE = "(?P<octave>'+|,+)"
+RE_REST = "(?P<rest>r)"
+RE_DURATION = "(?P<duration>\d+\.*)"
+RE_SHARP = "(?P<sharp>(is)*)"
+RE_FLAT = "(?P<flat>(es)*)"
+RE_ACCIDENTALS = "(%s%s)?"%(RE_SHARP, RE_FLAT)
 
 def tokenize(s):
     return s.split()
@@ -18,14 +26,20 @@ def parse(s):
     duration = 16
     curr_octave = 4
     offset = 0
+    token_matcher = "(%s%s?%s|%s)%s?"%(RE_NOTE, RE_8VE, RE_ACCIDENTALS, RE_REST,
+            RE_DURATION)
     
     for token in tokenize(s):
-        m = re.match("((?P<note>[abcdefg])(?P<octave>'+|,+)?|(?P<rest>r))(?P<duration>\d+\.*)?", token)
+        m = re.match(token_matcher, token)
         if m:
             note = m.groupdict()["note"]
             octave_marker = m.groupdict()["octave"]
             duration_marker = m.groupdict()["duration"]
+            accidental_sharp = m.groupdict()["sharp"]
+            accidental_flat = m.groupdict()["flat"]
             rest = m.groupdict()["rest"]
+            accidental_change = 0
+            print m.groupdict()
             if duration_marker is None:
                 pass # leave duration the way it was
             elif duration_marker == "1":
@@ -55,7 +69,12 @@ def parse(s):
                     octave = curr_octave - 2
                 else:
                     octave = curr_octave
-                note_value = MIDI_NOTE_VALUES[note] + (12 * octave)
+                if accidental_sharp:
+                    accidental_change+=len(accidental_sharp)/2
+                if accidental_flat:
+                    accidental_change-=len(accidental_flat)/2
+                note_value = MIDI_NOTE_VALUES[note] + (12 * octave) +\
+                        accidental_change
                 yield (offset, note_value, duration)
             offset += duration
         else:
