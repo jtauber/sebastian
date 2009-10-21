3b2cd4722dfdd741968f243780dda3431b04cf50
James Tauber <jtauber@jtauber.com>
2009-10-21 19:45:27 +0100

    beginning of an experiment for the next level of the algebra

---
+++ experimental/alberti.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+## this is the beginning of an experiment for the next level of the algebra
+
+import sys
+sys.path.append("..")
+
+from core import MIDI_PITCH, OFFSET_64, DURATION_64
+from core import Sequence, Point
+
+from midi.player import play
+
+C = Sequence([Point({MIDI_PITCH: 60})])
+E = Sequence([Point({MIDI_PITCH: 64})])
+G = Sequence([Point({MIDI_PITCH: 67})])
+
+C_major_root = C // E // G
+
+def alberti(triad, duration):
+    return Sequence([
+        Point({OFFSET_64: 0 * duration, MIDI_PITCH: triad[0][MIDI_PITCH], DURATION_64: duration}),
+        Point({OFFSET_64: 1 * duration, MIDI_PITCH: triad[2][MIDI_PITCH], DURATION_64: duration}),
+        Point({OFFSET_64: 2 * duration, MIDI_PITCH: triad[1][MIDI_PITCH], DURATION_64: duration}),
+        Point({OFFSET_64: 3 * duration, MIDI_PITCH: triad[2][MIDI_PITCH], DURATION_64: duration}),
+    ])
+
+play(alberti(C_major_root, 8) * 16)
