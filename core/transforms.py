5f0df4995d99b93e7318500091008fc1c32c1011
James Tauber <jtauber@jtauber.com>
2009-10-21 12:15:53 +0100

    fix transpose to support points without pitches

--- core/transforms.py
+++ core/transforms.py
@@ -3,13 +3,13 @@ from core import MIDI_PITCH, OFFSET_64
 
 def transpose(semitones):
     def _(point):
-        point[MIDI_PITCH] = point[MIDI_PITCH] + semitones
+        if MIDI_PITCH in point:
+            point[MIDI_PITCH] = point[MIDI_PITCH] + semitones
         return point
     return _
 
 
 def shift(offset):
-    
     def _(point):
         point[OFFSET_64] = point[OFFSET_64] + offset
         return point
