a9c84b0fb567302e3515e8e438e3b408e0852536
James Tauber <jtauber@jtauber.com>
2009-10-21 14:23:14 +0100

    added unit test for stretch

--- core/tests.py
+++ core/tests.py
@@ -97,7 +97,7 @@ assert s2 // s3 == [
 
 ## point transformation
 
-from core.transforms import transpose, reverse
+from core.transforms import transpose, reverse, stretch
 
 assert s1 | transpose(12) == [
     {MIDI_PITCH: 62, OFFSET_64: 16, DURATION_64: 16},
@@ -109,4 +109,9 @@ assert s1 | transpose(5) | transpose(-5) == s1
 assert s1 | reverse() == [
     {MIDI_PITCH: 52, OFFSET_64: 0, DURATION_64: 16},
     {MIDI_PITCH: 50, OFFSET_64: 16, DURATION_64: 16}
+]
+
+assert s1 | stretch(2) == [
+    {MIDI_PITCH: 50, OFFSET_64: 32, DURATION_64: 32},
+    {MIDI_PITCH: 52, OFFSET_64: 64, DURATION_64: 32}
 ]
