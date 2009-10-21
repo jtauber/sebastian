d7ec24adaa1ed96707ba1eca6bc138abcd64d449
James Tauber <jtauber@jtauber.com>
2009-10-21 14:15:43 +0100

    added reverse transform

--- core/transforms.py
+++ core/transforms.py
@@ -1,4 +1,6 @@
 from core import MIDI_PITCH, OFFSET_64, DURATION_64
+from core import Point, Sequence
+
 
 def transpose(semitones):
     def _(point):
@@ -15,3 +17,15 @@ def stretch(multiplier):
             point[DURATION_64] = int(point[DURATION_64] * multiplier)
         return point
     return lambda seq: seq.map_points(_)
+
+
+def reverse():
+    def _(sequence):
+        new_sequence = []
+        last_offset = sequence.last_point()[OFFSET_64]
+        for point in sorted(sequence, key=lambda x: x[OFFSET_64], reverse=True):
+            new_point = Point(point)
+            new_point[OFFSET_64] = last_offset - new_point[OFFSET_64]
+            new_sequence.append(new_point)
+        return new_sequence
+    return _
