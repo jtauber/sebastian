a9e9665e823478cdf5a42e13d7137699103b7f47
James Tauber <jtauber@jtauber.com>
2009-10-21 11:16:50 +0100

    added midi.player module so you can play sequences from the interactive shell (at least in OS X)

---
+++ midi/player.py
@@ -0,0 +1,22 @@
+## The goal of this module is to eventually be to MIDI players what
+## 'webbrowser' is to Web browsers.
+##
+## Personally, I only need it to work on OS X so patches accepted for other
+## operating systems.
+
+import sys
+import tempfile
+import subprocess
+import write_midi
+
+def play(sequence):
+    if sys.platform == "darwin":
+        
+        f = tempfile.NamedTemporaryFile(suffix=".mid", delete=False)
+        s = write_midi.SMF(sequence)
+        s.write(f)
+        f.close()
+        
+        subprocess.call(["open", f.name])
+    else:
+        print "Only OS X supported at the moment. Patches accepted!"
