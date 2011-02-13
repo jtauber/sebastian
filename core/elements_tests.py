335d137b525bc3dcec384c794ba459565d9cb952
James Tauber <jtauber@jtauber.com>
2011-02-13 06:59:20 -0500

    refactored the point/sequence objects into an elements module, rewriting a lot of it along the way

---
+++ core/elements_tests.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+
+from elements import Point, OSeq, UnificationError
+
+p1 = Point(a=1, b="foo")
+
+p2 = Point(a=1, b="foo")
+
+assert p1 == p2
+
+p3 = Point(c=2)
+
+assert p1 != p3
+
+p4 = p1 % p3
+
+assert p4 == Point(a=1, b="foo", c=2)
+
+assert p1 % p2 == p1
+
+p5 = Point(a=2)
+
+try:
+    p1 % p5
+    assert False
+except UnificationError:
+    assert True
+
+OffsetSequence = OSeq("offset", "duration")
+
+s1 = OffsetSequence()
+s2 = OffsetSequence([p1, p4])
+s3 = OffsetSequence(p1, p4)
+s4 = OffsetSequence(p1) + OffsetSequence(p4)
+
+assert s4 == s3
+
+s5 = OffsetSequence(Point(duration=10), Point(duration=10))
+
+assert s5._elements[1]["offset"] == 10
+
+s6 = OffsetSequence(p1) * 5
+
+assert len(s6._elements) == 5
+assert s6._elements[-1]["offset"] == 0
+
+s7 = s5 * 2
+
+assert len(s7._elements) == 4
+assert s7._elements[-1]["offset"] == 30
+
+s8 = OffsetSequence(Point(a=1, offset=0), Point(a=2, offset=20)) // OffsetSequence(Point(a=3, offset=10))
+
+assert s8._elements[1]["a"] == 3
--- core/tests.py
+++ core/tests.py
@@ -27,7 +27,7 @@ p2 = Point({
 
 s1 = OSequence([p1, p2])
 
-assert s1.to_list() == [
+assert s1._elements == [
     {DURATION_64: 16, OFFSET_64: 16, MIDI_PITCH: 50},
     {DURATION_64: 16, OFFSET_64: 32, MIDI_PITCH: 52}
 ]
@@ -43,24 +43,23 @@ assert OSequence([]).last_point() == {
 
 ## concatenation
 
-assert (s1 + s1).to_list() == [
+assert (s1 + s1)._elements == [
     {DURATION_64: 16, OFFSET_64: 16, MIDI_PITCH: 50},
     {DURATION_64: 16, OFFSET_64: 32, MIDI_PITCH: 52},
     {DURATION_64: 16, OFFSET_64: 64, MIDI_PITCH: 50},
     {DURATION_64: 16, OFFSET_64: 80, MIDI_PITCH: 52}
 ]
-
 
 ## repetition
 
-assert (s1 * 2).to_list() == [
+assert (s1 * 2)._elements == [
     {DURATION_64: 16, OFFSET_64: 16, MIDI_PITCH: 50},
     {DURATION_64: 16, OFFSET_64: 32, MIDI_PITCH: 52},
     {DURATION_64: 16, OFFSET_64: 64, MIDI_PITCH: 50},
     {DURATION_64: 16, OFFSET_64: 80, MIDI_PITCH: 52}
 ]
 
-assert (s1 * 3).to_list() == [
+assert (s1 * 3)._elements == [
     {DURATION_64: 16, OFFSET_64: 16, MIDI_PITCH: 50},
     {DURATION_64: 16, OFFSET_64: 32, MIDI_PITCH: 52},
     {DURATION_64: 16, OFFSET_64: 64, MIDI_PITCH: 50},
@@ -87,7 +86,7 @@ p4 = Point({
 s2 = OSequence([p3]) * 4
 s3 = OSequence([p4]) * 8
 
-assert (s2 // s3).to_list() == [
+assert (s2 // s3)._elements == [
     {MIDI_PITCH: 64, OFFSET_64: 0, DURATION_64: 16},
     {MIDI_PITCH: 66, OFFSET_64: 0, DURATION_64: 8},
     {MIDI_PITCH: 66, OFFSET_64: 8, DURATION_64: 8},
@@ -109,7 +108,7 @@ from core.transforms import transpose, reverse, stretch, invert
 
 # transpose
 
-assert (s1 | transpose(12)).to_list() == [
+assert (s1 | transpose(12))._elements == [
     {MIDI_PITCH: 62, OFFSET_64: 16, DURATION_64: 16},
     {MIDI_PITCH: 64, OFFSET_64: 32, DURATION_64: 16}
 ]
@@ -118,7 +117,7 @@ assert s1 | transpose(5) | transpose(-5) == s1
 
 # reverse
 
-assert (s1 | reverse()).to_list() == [
+assert (s1 | reverse())._elements == [
     {MIDI_PITCH: 52, OFFSET_64: 0, DURATION_64: 16},
     {MIDI_PITCH: 50, OFFSET_64: 16, DURATION_64: 16},
     {OFFSET_64: 48}
@@ -128,7 +127,7 @@ assert s1 | reverse() | reverse() == s1
 
 # stretch
 
-assert (s1 | stretch(2)).to_list() == [
+assert (s1 | stretch(2))._elements == [
     {MIDI_PITCH: 50, OFFSET_64: 32, DURATION_64: 32},
     {MIDI_PITCH: 52, OFFSET_64: 64, DURATION_64: 32}
 ]
@@ -137,7 +136,7 @@ assert s1 | stretch(2) | stretch(0.5) == s1
 
 # invert
 
-assert (s1 | invert(50)).to_list() == [
+assert (s1 | invert(50))._elements == [
     {DURATION_64: 16, OFFSET_64: 16, MIDI_PITCH: 50},
     {DURATION_64: 16, OFFSET_64: 32, MIDI_PITCH: 48}
 ]
