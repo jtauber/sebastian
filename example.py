dd24eff6b58a705c93376e572b48f70f0dc78dca
James Tauber <jtauber@jtauber.com>
2011-02-13 08:40:42 -0500

    added more examples

--- example.py
+++ example.py
@@ -2,7 +2,9 @@
 
 from lilypond.interp import parse
 from midi import write_midi, player
-from core.transforms import transpose, reverse, stretch, invert
+from core import OSequence, HSeq, Point, DURATION_64
+from core.transforms import transpose, reverse, stretch, invert, add, degree_in_key, midi_pitch
+from core.notes import Key, major_scale
 
 # construct sequences using lilypond syntax
 seq1 = parse("c d e")
@@ -25,3 +27,13 @@ f = open("seq5.mid", "w")
 s = write_midi.SMF(seq5)
 s.write(f)
 f.close()
+
+# contruct a horizontal sequence of scale degrees
+seq6 = HSeq([Point(degree=degree) for degree in [1, 2, 3, 2, 1]])
+
+# put that sequence into C major, octave 4 quavers
+C_MAJOR = Key("C", major_scale)
+seq7 = seq6 | add({"octave": 4, DURATION_64: 8}) | degree_in_key(C_MAJOR)
+
+# convert to MIDI pitch and play
+player.play(OSequence(seq7 | midi_pitch()))
