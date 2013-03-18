debc5a6548b1f38c5faefdce7c479a682f1a63ee
James Tauber <jtauber@jtauber.com>
2013-03-18 05:56:18 -0700

    bumped version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "sebastian",
-    version = "0.0.2",
+    version = "0.0.3",
     description = "symbolic music analysis and composition library in Python",
     license = "MIT",
     url = "http://github.com/jtauber/sebastian",
