d3911dc8418fe6ace3ff97746a90169b891df387
Caleb Smith <csmith@caktusgroup.com>
2013-03-18 00:34:10 -0400

    Added support for GNU/Linux using Timidity

--- sebastian/midi/player.py
+++ sebastian/midi/player.py
@@ -12,13 +12,13 @@ from sebastian.midi import write_midi
 
 
 def play(tracks):
+    f = tempfile.NamedTemporaryFile(suffix=".mid", delete=False)
+    s = write_midi.SMF(tracks)
+    s.write(f)
+    f.close()
     if sys.platform == "darwin":
-        
-        f = tempfile.NamedTemporaryFile(suffix=".mid", delete=False)
-        s = write_midi.SMF(tracks)
-        s.write(f)
-        f.close()
-        
         subprocess.call(["open", f.name])
+    elif sys.platform == "linux2":
+        subprocess.call(["timidity", f.name])
     else:
         print "Only OS X supported at the moment. Patches accepted!"
