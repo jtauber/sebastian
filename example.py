d9e33ff376c747990d19bf99dadcd0746036f64a
James Tauber <jtauber@jtauber.com>
2013-03-19 15:33:37 -0700

    moved lilypoint writing mainline to examples

--- example.py
+++ example.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
 
 from sebastian.lilypond.interp import parse
+from sebastian.lilypond.write_lilypond import write
 from sebastian.midi import write_midi, player
 from sebastian.core import OSequence, HSeq, Point, DURATION_64
-from sebastian.core.transforms import transpose, reverse, add, degree_in_key, midi_pitch
+from sebastian.core.transforms import transpose, reverse, add, degree_in_key, midi_pitch, lilypond
 from sebastian.core.notes import Key, major_scale
 
 # construct sequences using lilypond syntax
@@ -34,3 +35,19 @@ seq7 = seq6 | add({"octave": 4, DURATION_64: 8}) | degree_in_key(C_MAJOR)
 
 # convert to MIDI pitch and play
 player.play([OSequence(seq7 | midi_pitch())])
+
+
+# sequence of first four degree of a scale
+seq8 = HSeq(Point(degree=n) for n in [1, 2, 3, 4])
+
+# add duration and octave
+seq8 = seq8 | add({DURATION_64: 16, "octave": 5})
+
+# put into C major
+seq8 = seq8 | degree_in_key(C_MAJOR)
+
+# annotate with lilypond
+seq8 = seq8 | lilypond()
+
+# write out lilypond file
+write("example.ly", seq8)
--- sebastian/lilypond/write_lilypond.py
+++ sebastian/lilypond/write_lilypond.py
@@ -1,5 +1,3 @@
-
-
 def output(seq):
     return "{ %s }" % " ".join(point["lilypond"] for point in seq)
 
@@ -7,19 +5,3 @@ def output(seq):
 def write(filename, seq):
     with open(filename, "w") as f:
         f.write(output(seq))
-
-
-if __name__ == "__main__":
-    from sebastian.core import DURATION_64
-    from sebastian.core import HSeq, Point
-    from sebastian.core.notes import Key, major_scale
-    from sebastian.core.transforms import degree_in_key, add, lilypond
-
-    seq = HSeq(Point(degree=n) for n in [1, 2, 3, 4])
-    seq = seq | add({DURATION_64: 16, "octave": 5})
-
-    C_major = Key("C", major_scale)
-
-    seq = seq | degree_in_key(C_major) | lilypond()
-
-    write("test.ly", seq)
