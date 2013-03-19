5f06d7affd723654622663eeb225d9676d2cb563
James Tauber <jtauber@jtauber.com>
2013-03-19 16:04:07 -0700

    use absolute path for test midi filename

--- tests/test_midi.py
+++ tests/test_midi.py
@@ -6,8 +6,10 @@ from sebastian.midi.midi import load_midi
 class TestMidi(TestCase):
 
     def test_load_midi(self):
+        import os.path
+        filename = os.path.join(os.path.dirname(__file__), "scale.mid")
         self.assertEqual(
-            list(load_midi("scale.mid")[0]),
+            list(load_midi(filename)[0]),
             [
                 {'midi_pitch': 60, 'offset_64': 42, 'duration_64': 15},
                 {'midi_pitch': 62, 'offset_64': 56, 'duration_64': 7},
